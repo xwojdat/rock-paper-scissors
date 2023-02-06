5ce066b42d5a3f9d2dc9258cbe15fb2c06a1b699
xwojdat <123936050+xwojdat@users.noreply.github.com>
2023-02-06 10:19:56 +0000

    working game

--- main.js
+++ main.js
@@ -1,80 +1,78 @@
-const options = document.querySelectorAll('.options');
+const options = document.querySelectorAll(".options");
 let pScore = 0;
 let cScore = 0;
 
 options.forEach((option) => {
-
-});
-
-options.forEach((option) => {
-    option.addEventListener('click', function () {
+    option.addEventListener("click", function () {
         const pInput = this.textContent;
 
-        const cOptions = ["Rock", "Paper", "Scissors"];
-        const cInput = cOptions[Math.floor(Math.random()*3)];
+        const cOptions = ["Kamień", "Papier", "Nożyce"];
+        const cInput = cOptions[Math.floor(Math.random() * 3)];
 
-        compare(pInput, cInput);
+        compareInputs(pInput, cInput);
         updateScore();
-
-        if(checkWinner()){
+        if (checkWinner()) {
             pScore = cScore = 0;
             updateScore();
         }
     });
 });
 
-function updateScore() {
-    document.getElementById('pScore').textContent = pScore;
-    document.getElementById('cScore').textContent = cScore;
+function compareInputs(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+
+    if (pInput === cInput) {
+        alert(`${currentMatch} Remis`);
+        return;
+    } 
+    
+    else if (pInput === "Kamień") {
+        if (cInput === "Nożyce") {
+            alert(`${currentMatch} = Wygrałeś!`);
+            pScore++;
+        } 
+        else {
+            alert(`${currentMatch} = Steve Wygrał`);
+            cScore++;
+        }
+    } 
+    
+    else if (pInput === "Papier") {
+        if (cInput === "Kamień") {
+            alert(`${currentMatch} = Wygrałeś!`);
+            pScore++;
+        } 
+        else {
+            alert(`${currentMatch} = Steve Wygrał`);
+            cScore++;
+        }
+    } 
+    
+    else {
+        if (cInput === "Papier") {
+            alert(`${currentMatch} = Wygrałeś`);
+            pScore++;
+        } 
+        else {
+            alert(`${currentMatch} = Steve Wygrał`);
+            cScore++;
+        }
+    }
 }
+
+function updateScore() {
+    document.getElementById("p-score").textContent = pScore;
+    document.getElementById("c-score").textContent = cScore;
+  }
 
 function checkWinner() {
     if(pScore === 3 || cScore === 3){
         const winner = 
             pScore === 3
-                ?"You Win!"
-                :"You Lost!";
+                ?"Wygrałeś! Dobra robota!"
+                :"Steve wygrał! Powodzenia następnym razem!";
             alert(winner);
             return true;
     }
     return false;
 }
-
-function compare(pInput, cInput) {
-    const currentMatch = `${pInput} vs ${cInput}`;
-
-    if (pInput === cInput) {
-        return "It's a tie!";
-    } 
-    
-    else if (pInput === "Rock") {
-        if (cInput === "Scissors") {
-            return "You win!";
-            pScore++;
-        } else {
-            return "You lose!";
-            cScore++;
-        }
-    } 
-    
-    else if (pInput === "Paper") {
-        if (cInput === "Rock") {
-            return "You win!";
-            pScore++;
-        } else {
-            return "You lose!";
-            cScore++;
-        }
-    } 
-    
-    else if (pInput === "Scissors") {
-        if (cInput === "Paper") {
-            return "You win!";
-            pScore++;
-        } else {
-            return "You lose!";
-            cScore++;
-        }
-    }
-}
-
