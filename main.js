ecdedc728cb1a53b5e951ae6a491aaddb6381348
xwojdat <123936050+xwojdat@users.noreply.github.com>
2023-02-06 09:41:13 +0000

    game mechanism

--- main.js
+++ main.js
@@ -6,5 +6,39 @@ options.forEach((option) => {
 
         const cOptions = ["Rock", "Paper", "Scissors"];
         const cInput = cOptions[Math.floor(Math.random()*3)];
+
+        compare(pInput, cInput);
     });
 });
+
+function compare(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+
+    if (pInput === cInput) {
+        return "It's a tie!";
+    } 
+    
+    else if (pInput === "Rock") {
+        if (cInput === "Scissors") {
+            return "You win!";
+        } else {
+            return "You lose!";
+        }
+    } 
+    
+    else if (pInput === "Paper") {
+        if (cInput === "Rock") {
+            return "You win!";
+        } else {
+            return "You lose!";
+        }
+    } 
+    
+    else if (pInput === "Scissors") {
+        if (cInput === "Paper") {
+            return "You win!";
+        } else {
+            return "You lose!";
+        }
+    }
+}
