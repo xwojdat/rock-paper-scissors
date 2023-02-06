d69ac07589ed519302231f53647cc8915e7e0192
xwojdat <123936050+xwojdat@users.noreply.github.com>
2023-02-06 09:52:14 +0000

    score record

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const options = document.querySelectorAll('.options');
+let pScore = 0;
+let cScore = 0;
+
+options.forEach((option) => {
+
+});
 
 options.forEach((option) => {
     option.addEventListener('click', function () {
@@ -8,8 +14,31 @@ options.forEach((option) => {
         const cInput = cOptions[Math.floor(Math.random()*3)];
 
         compare(pInput, cInput);
+        updateScore();
+
+        if(checkWinner()){
+            pScore = cScore = 0;
+            updateScore();
+        }
     });
 });
+
+function updateScore() {
+    document.getElementById('pScore').textContent = pScore;
+    document.getElementById('cScore').textContent = cScore;
+}
+
+function checkWinner() {
+    if(pScore === 3 || cScore === 3){
+        const winner = 
+            pScore === 3
+                ?"You Win!"
+                :"You Lost!";
+            alert(winner);
+            return true;
+    }
+    return false;
+}
 
 function compare(pInput, cInput) {
     const currentMatch = `${pInput} vs ${cInput}`;
@@ -21,24 +50,31 @@ function compare(pInput, cInput) {
     else if (pInput === "Rock") {
         if (cInput === "Scissors") {
             return "You win!";
+            pScore++;
         } else {
             return "You lose!";
+            cScore++;
         }
     } 
     
     else if (pInput === "Paper") {
         if (cInput === "Rock") {
             return "You win!";
+            pScore++;
         } else {
             return "You lose!";
+            cScore++;
         }
     } 
     
     else if (pInput === "Scissors") {
         if (cInput === "Paper") {
             return "You win!";
+            pScore++;
         } else {
             return "You lose!";
+            cScore++;
         }
     }
 }
+
