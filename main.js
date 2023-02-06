3d484c1f4d378c23147075d34d5260be21b0e51d
xwojdat <123936050+xwojdat@users.noreply.github.com>
2023-02-06 09:20:18 +0000

    new game

--- main.js
+++ main.js
@@ -1,68 +1,10 @@
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    function playGame(){
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelector('.paper');
-        const scissorsBtn = document.querySelector('.scissors');
-        const playerOptions = [rockBtn, paperBtn, scissorsBtn]
+const options = document.querySelectorAll('.options');
 
-        playerOptions.forEach(option => {
-            option.addEventListener('click',function(){
-             
-                const choiceNumber = Math.floor(Math.random()*3);
-                const computerChoice = computerOptions[choiceNumber];
+options.forEach((option) => {
+    option.addEventListener('click', function () {
+        const pInput = this.textContent;
 
-                winner(this.innerText,computerChoice);
-            })
-        })
-    }
-
-    function winner (player, computer){
-        const result = document.querySelector('.result');
-        const playerScoreBoard = document.querySelector('.p-count');
-        const computerScoreBoard = document.querySelector('.c-count');
-        player = player.toLowerCase();
-        computer = computer.toLowerCase();
-        if(player === computer){
-            result.textContent = 'Remis'
-        }
-
-        else if(player === 'rock'){
-            if(computer == 'paper'){
-                result.textContent = 'Komputer Wygrał';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }
-            else{
-                result.textContent = 'Wygrałeś';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if(player === 'paper'){
-            if(computer == 'rock'){
-                result.textContent = 'Wygrałeś';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-            else{
-                result.textContent = 'Komputer Wygrał';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }
-        }
-        else if(player ==='scissors'){
-            if(computer == 'paper'){
-                result.textContent = 'Wygrałeś';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }
-            else{
-                result.textContent = 'Komputer Wygrał';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-    }
-}
+        const cOptions = ["Rock", "Paper", "Scissors"];
+        const cInput = cOptions[Math.floor(Math.random()*3)];
+    });
+});
